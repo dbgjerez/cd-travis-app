897ba13aa4a60bde497a05e8876bd1353100bd77
David Borrego <dborrego@atsistemas.com>
2018-10-23 09:50:05 +0200

    init repo

---
+++ src/test/java/com/dbg/travisapp/TravisAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dbg.travisapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TravisAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
