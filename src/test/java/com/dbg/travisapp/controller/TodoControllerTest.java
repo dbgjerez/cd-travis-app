28f176b17627273963093741b6fdea1fc38bb1dc
David Borrego <dborrego@atsistemas.com>
2018-11-08 09:43:56 +0100

    test completo controller

--- src/test/java/com/dbg/travisapp/controller/TodoControllerTest.java
+++ src/test/java/com/dbg/travisapp/controller/TodoControllerTest.java
@@ -8,17 +8,20 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import java.util.Arrays;
 import java.util.Optional;
 
 import static com.dbg.travisapp.dummy.Dummy.toJSON;
 import static org.hamcrest.CoreMatchers.containsString;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -27,6 +30,7 @@ public class TodoControllerTest {
 
   private static final Integer TODO_ID = 1;
   private static final String TITLE = "Prueba";
+  private static final String URL_PATH = "/todo";
 
   private MockMvc mockMvc;
 
@@ -63,4 +67,35 @@ public class TodoControllerTest {
                 .content(toJSON(TodoDummy.create(TITLE, Boolean.TRUE))))
         .andExpect(status().isOk());
   }
+
+  @Test
+  public void testCreate() throws Exception {
+    when(todoService.create(any())).thenReturn(TodoDummy.create(TODO_ID, TITLE, Boolean.FALSE));
+    mockMvc
+        .perform(
+            post("/todo")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(toJSON(TodoDummy.create())))
+        .andExpect(status().isOk())
+        .andExpect(content().string(containsString(TITLE)))
+        .andExpect(content().string(containsString(TODO_ID.toString())));
+  }
+
+  @Test
+  public void testDelete() throws Exception {
+    mockMvc
+        .perform(delete("/todo/" + TODO_ID).contentType(MediaType.APPLICATION_JSON))
+        .andExpect(status().isOk());
+    verify(todoService).delete(TODO_ID);
+  }
+
+  @Test
+  public void testFindAll() throws Exception {
+    when(todoService.findAll(any(), any()))
+        .thenReturn(new PageImpl<>(Arrays.asList(TodoDummy.create(), TodoDummy.create())));
+    mockMvc
+        .perform(get(URL_PATH).contentType(MediaType.APPLICATION_JSON))
+        .andExpect(status().isOk());
+  }
+
 }
--- src/test/java/com/dbg/travisapp/dummy/TodoDummy.java
+++ src/test/java/com/dbg/travisapp/dummy/TodoDummy.java
@@ -9,6 +9,12 @@ public class TodoDummy extends Dummy {
     return create(Strings.EMPTY, Boolean.FALSE);
   }
 
+  public static Todo create(Integer id, String title, Boolean done) {
+    final Todo todo = create(title, done);
+    todo.setId(id);
+    return todo;
+  }
+
   public static Todo create(String title, Boolean done) {
     final Todo todo = new Todo();
     todo.setTitle(title);
